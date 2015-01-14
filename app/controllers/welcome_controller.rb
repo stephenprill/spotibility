5bd122f7e11e888a319aab02e0e956120ec27378
Stephen Prill <stephen.prill@gmail.com>
2015-01-14 16:32:52 -0700

    added people controller and got index working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,12 @@ class WelcomeController < ApplicationController
   require 'rspotify'
 
   def index
+    if params[:q]
     @artists = RSpotify::Artist.search(params[:q])
+  else
+    render :index
   end
+end
 
   # def search
   #   @artists = RSpotify::Artist.search(params[:q])
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  
   # w / joe --  get '/welcome/search' => 'welcome#search'
 
   # Example of regular route:
