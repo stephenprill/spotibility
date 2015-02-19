e5fd134190d07a2cc08e982d39ba21c3aee1b579
Stephen Prill <stephen.prill@gmail.com>
2015-02-18 17:08:02 -0700

    spotify api working now and added users model and omauth working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,44 @@
 class UsersController < ApplicationController
+
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     session[:token] = request.env['omniauth.auth']['credentials']['token']
+    user = User.find_or_initialize_by(spotify_id: request.env['omniauth.auth']['extra']['raw_info']['id'])
+    user.email = request.env['omniauth.auth']['extra']['raw_info']['email']
+    user.save!
+    session[:user_id] = user.id
     redirect_to users_path
   end
 
   def index
-    @user1 = RSpotify::User.find('stephenprill')
+    @users = User.all
+    # current_user = find the user by the session :id
+    # get the user's spotify id (which is the currently logged in user's spotify id)
+    # @tracks = call_spotify("/v1/users/#{@user.spotify_id}/playlists")
+    @tracks = call_spotify("/v1/me")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    @playlists = call_spotify("/v1/users/#{@user.spotify_id}/playlists")
+
+    @tracks = {}
+
+    @playlists[:items].each do |playlist|
+      tracks = call_spotify("/v1/users/#{@user.spotify_id}/playlists/#{playlist[:id]}/tracks")[:items]
+      @tracks[playlist[:id]] = tracks
+    end
+  end
+
+  private
+
+  def call_spotify(path)
+    token = session[:token]
+    conn = Faraday.new(:url => 'https://api.spotify.com/')
+    response = conn.get(path) do |req|
+      req.headers['Authorization'] = "Bearer #{token}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- junk.rb
+++ junk.rb
@@ -1,11 +1,10 @@
-require 'faraday'
-
-token = "BQCWoVpOwD_JACHPN4vWPC7eit2t40S1IsY1yA6gIEJwuqIhpsJqy2ijp2ogFJxIVKKFTPLvGGjVjAMDcNkqFGVTe-Er2ct1FBSc3WOudni9tr2zKRy5DUA5KD8HPZ86Qb6itSnEo7Z7EDBIC-O-pt_qCXF9RRr6ZNLYkFrahoI_HueEgKocNWSlanYzJN-xs0BsAxNER-27hA"
-
-conn = Faraday.new(:url => 'https://api.spotify.com/')
-
-response = conn.get("/v1/me") do |req|
-  req.headers['Authorization'] = "Bearer #{token}"
-end
-
-puts response.body
+# require 'faraday'
+#
+# token = "BQCWoVpOwD_JACHPN4vWPC7eit2t40S1IsY1yA6gIEJwuqIhpsJqy2ijp2ogFJxIVKKFTPLvGGjVjAMDcNkqFGVTe-Er2ct1FBSc3WOudni9tr2zKRy5DUA5KD8HPZ86Qb6itSnEo7Z7EDBIC-O-pt_qCXF9RRr6ZNLYkFrahoI_HueEgKocNWSlanYzJN-xs0BsAxNER-27hA"
+# conn = Faraday.new(:url => 'https://api.spotify.com/')
+#
+# response = conn.get("/v1/me") do |req|
+#   req.headers['Authorization'] = "Bearer #{token}"
+# end
+#
+# puts response.body
