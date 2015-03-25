f34a73bab58f7fb0cca5e94c6807ab2d3b59e92c
Stephen Prill <stephen.prill@gmail.com>
2015-03-25 01:15:52 -0600

    moved ensure_current_user to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :ensure_current_user, except: :spotify
+
+  def ensure_current_user
+    unless current_user
+      session[:first_url] = request.url if request.get?
+      redirect_to root_path, notice: 'You must be logged in to access that action'
+    end
+  end
+
   def current_user
       User.find_by(id: session[:user_id])
   end
+
+
 
   helper_method :current_user
 end
